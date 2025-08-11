5b958fd76bb38c64c4d5e4d406f8878b60197b75
NIYAMOT ULLAH <NIYAMOTULLAH8905@GMAIL.COM>
2025-08-11 19:57:33 +0530

    Add files via upload

---
+++ banks_list.js
@@ -0,0 +1,13 @@
+
+const banks = [
+    "State Bank of India",
+    "Punjab National Bank",
+    "Bank of Baroda",
+    "Canara Bank",
+    "HDFC Bank",
+    "ICICI Bank",
+    "Axis Bank",
+    "Union Bank of India",
+    "Kotak Mahindra Bank",
+    "Yes Bank"
+];
